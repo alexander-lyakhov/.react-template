a20c0b03c55a78d1ac8749855c901b5589049d65
alexander-lyakhov <alexr.lyakhov@gmail.com>
2018-06-10 16:40:30 +0300

    first commit

---
+++ src/components/app.jsx
@@ -0,0 +1,11 @@
+﻿import React from 'react';
+import logo from '../assets/react-logo.png';
+
+export default function() {
+    return (
+        <div className="screen-center">
+            <img src={logo} width="256" />
+        </div>
+    )
+}
+
